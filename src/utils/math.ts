3bddda4d0140c8d239ab9557d5e0a23e56b8a6a4
alphardex <2582347430@qq.com>
2021-10-16 20:00:11 +0800

    update

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -54,6 +54,19 @@ const sphube = (u1, v1, target) => {
 
 const percent = (value: number, total: number) => (value / total) * 100;
 
+// 获取重心坐标系
+const getBaryCoord = (bufferGeometry: THREE.BufferGeometry) => {
+  // https://gist.github.com/mattdesl/e399418558b2b52b58f5edeafea3c16c
+  const length = bufferGeometry.attributes.position.array.length;
+  const count = length / 3;
+  const bary = [];
+  for (let i = 0; i < count; i++) {
+    bary.push(0, 0, 1, 0, 1, 0, 1, 0, 0);
+  }
+  const aCenter = new Float32Array(bary);
+  bufferGeometry.setAttribute("aCenter", new THREE.BufferAttribute(aCenter, 3));
+};
+
 export {
   calcAspect,
   Point,
@@ -65,4 +78,5 @@ export {
   TAU,
   sphube,
   percent,
+  getBaryCoord,
 };
